74c0bf5211da4980c69e84bb8820f6cb944bc836
Gilbike <marton.dikacz@gmail.com>
2024-10-03 09:29:45 +0200

    feladatok

--- f2/f2.js
+++ f2/f2.js
@@ -1,8 +1,15 @@
-const students = require('../students.json');
+const students = require("../students.json");
 /* Írj függvényt ami feltölt egy objektumot (lehetőleg üres {}-ről induljon) 
 a klubbok nevével és tagjainak számával. (pl. {"club1": 4, "club2": 3....}) */
 function f2() {
-    //TODO
+  const clubs = {};
+  students.forEach((student) => {
+    if (clubs[student.club] === undefined) {
+      clubs[student.club] = 0;
+    }
+    clubs[student.club]++;
+  });
+  return clubs;
 }
 
 module.exports = f2;
--- f3/f3.js
+++ f3/f3.js
@@ -1,9 +1,16 @@
-const students = require('../students.json');
+const students = require("../students.json");
 /* Írj függvényt ami megvizsgálja, hogy van-e eltérés a freeDormitory
  boolean és dormitoryPrice number-ek között 
  (Ha jól van feltöltve a lista a false értékek száma és a null-ok száma megegyezik.
   Ha nem akkor írja ki az eltérést) */
 const f3 = () => {
-    //TODO
-}
+  let diff = 0;
+
+  students.forEach((student) => {
+    if (student.freeDormitory == false) diff++;
+    if (student.dormitoryPrice == null) diff--;
+  });
+
+  return 0;
+};
 module.exports = f3;
